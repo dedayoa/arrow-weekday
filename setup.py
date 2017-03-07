261850c3ad8f2793d9ded1508628ae6862d79060
Dayo Ayeni <dedayoa@gmail.com>
2017-03-07 17:22:05 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import codecs
+import os.path
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='arrow_weekday',
+    version='0.0.1',
+    description='Weekdays for Arrow',
+    long_description=long_description,
+    url='https://github.com/dedayoa/arrow-weekday/',
+    author='Dayo Ayeni',
+    author_email="dedayoa@gmail.com",
+    license='MIT',
+    packages=['arrow-weekday'],
+    zip_safe=False,
+    install_requires=[
+        'arrow'
+    ],
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
